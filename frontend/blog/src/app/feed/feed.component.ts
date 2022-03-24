e5cff2db87a81a9b29bae344ccb5ce01382a4051
Charles Mesquita <charles_mesquita@kawasakibrasil.com>
2022-03-24 19:49:15 -0300

    editar feed component com injeção de dependencia do serviço para poder consumir o backend somente constructor

--- frontend/blog/src/app/feed/feed.component.ts
+++ frontend/blog/src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
   }
